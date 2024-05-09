1f514a961aabcc10255dbf2fe0b9f6e26c8ea939
irravasyuk <vasukirina29@gmail.com>
2024-05-09 13:54:22 +0300

    Initial commit

---
+++ 3.sql
@@ -0,0 +1,9 @@
+--SELECT * FROM VegetablesAndFruits
+--SELECT * FROM VegetablesAndFruits WHERE type = 'овоч'
+--SELECT * FROM VegetablesAndFruits WHERE type = 'фрукт'
+--SELECT name FROM VegetablesAndFruits
+--SELECT DISTINCT color FROM VegetablesAndFruits
+--SELECT * FROM VegetablesAndFruits WHERE type = 'фрукт' and color = 'жовтий'
+SELECT * FROM VegetablesAndFruits WHERE type = 'овоч' and color = 'зелений'
+
+
